b552384b9291b34ae7ffa325314b3b19ab8cee6d
Emre Isikligil <Emre.Isikligil@monitise.com>
2018-02-16 11:30:53 +0300

    Removed Copyright.

--- src/main/java/org/emreisikligil/validation/examples/service/DivideService.java
+++ src/main/java/org/emreisikligil/validation/examples/service/DivideService.java
@@ -1,20 +1,3 @@
-/*
- * MONITISE CONFIDENTIAL
- * ____________________
- * Copyright 2003 - 2015 Monitise Group Limited
- * All Rights Reserved. www.monitisegroup.com
- * NOTICE: All information contained herein is, and remains
- * the property of Monitise Group Limited or its group
- * companies. The intellectual and technical concepts contained
- * herein are proprietary to Monitise Group Limited and Monitise
- * group companies and may be covered by U.S. and
- * Foreign Patents, patents in process, and are protected by
- * trade secret or copyright law. Dissemination of this information
- * or reproduction of this material is strictly forbidden unless prior
- * written permission is obtained from Monitise Group Limited. Any
- * reproduction of this material must contain this notice
- */
-
 package org.emreisikligil.validation.examples.service;
 
 import org.emreisikligil.validation.examples.validator.AssertTotal;
